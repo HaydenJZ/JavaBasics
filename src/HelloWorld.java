c0ab21313263c32cf614b688b2e4b079344e1ad2
Jessica Hayden <jesshayden@jessicas-air.home>
2023-03-14 19:53:32 +0000

    First V2L

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // We will print the text "Hello World!"
+        System.out.print("Hello World!");
+
+            System.out.print(" and all the people of the world."); // We print more text to the same line
+    }
+}
